4ae419d8ba4fa745aef82bb48b6880f93b58a082
Justin Reiter <Justin.Reiter.1@gmail.com>
2024-09-23 11:15:42 -0700

    add skeleton for server and cl cog

---
+++ server.py
@@ -0,0 +1,3 @@
+### server.py
+# Basic webserver for acting as the intermediary between warzone & the clot
+# users will login through the webserver and the clot will interface with the webserver via authenticated rest API
