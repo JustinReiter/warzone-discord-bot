f8a81c192ab1c03e213876c40b566a6b64294eef
Justin Reiter <Justin.Reiter.1@gmail.com>
2025-01-01 02:11:05 -0500

    Archive data

--- cowboy_cl_scraper.py
+++ cowboy_cl_scraper.py
@@ -60,15 +60,36 @@ def game_matcher_CL9(game: FullWarzoneGame):
 
 
 def title_parser_CL9(game: FullWarzoneGame) -> Tuple[str, str, str]:
-    m = re.match(r"(CL\d*) \| (Group \w) - (.*)", game.title)
+    m = re.match(r"(CL\d*) \| (Group \w*) - (.*)", game.title)
     return m.group(1), m.group(2), m.group(3)
 
 
+def title_parser_CL10(game: FullWarzoneGame) -> Tuple[str, str, str]:
+    m = re.match(r"(CL\d*) ([\w ]*) :", game.title)
+    return m.group(1), "", m.group(2)
+
+
 def game_matcher_CL10(game: FullWarzoneGame):
-    return re.search(r"^CL9 ", game.title) and re.match(
+    return re.search(r"^CL10 ", game.title) and re.match(
         "This game has been created by the Clan League bot. If you fail to join it within 3 days, vote to end or decline, it will count as a loss",
         game.description,
     )
+
+
+def format_players_to_array(players: List[WarzonePlayer]):
+    winners = [
+        player for player in players if player.outcome == WarzonePlayer.Outcome.WON
+    ]
+    losers = [player for player in players if player not in winners]
+    extra_spaces = ["" for _ in range(6 - len(winners) * 2)]
+    slots: List[str] = []
+    for player in sorted(winners):
+        slots.extend([player.name, player.id])
+    slots.extend(extra_spaces)
+    for player in sorted(losers):
+        slots.extend([player.name, player.id])
+    slots.extend(extra_spaces)
+    return slots
 
 
 WARZONE_GAME_INDEXES = {
@@ -79,16 +100,16 @@ WARZONE_GAME_INDEXES = {
         "parser": title_parser_CL9,
     },
     "CL10": {
-        "start": 12820000,
-        "end": 14500000,
+        "start": 15350000,
+        "end": 16900001,
         "matcher": game_matcher_CL10,
-        "parser": title_parser_CL9,
+        "parser": title_parser_CL10,
     },
 }
 
 print("\n".join(sys.argv))
-if len(sys.argv) < 2:
-    raise "Invalid arguments provided. Expected `python3 cowboy_cl_scraper.py <CL9 | CL10>`"
+if len(sys.argv) < 3:
+    raise "Invalid arguments provided. Expected `python3 cowboy_cl_scraper.py <CL9 | CL10> <scrape | parse>`"
 
 cl_info = WARZONE_GAME_INDEXES[sys.argv[1]]
 if os.path.exists(f"data/ccs_index_{sys.argv[1]}"):
@@ -100,36 +121,58 @@ else:
     last_seen_index = cl_info["start"]
     warzone_data: List[ClotGame] = []
 
-try:
-    for i in range(last_seen_index, cl_info["end"]):
-        if i % 1000 == 0:
-            # Save entries every 1000 lines
-            overwrite_index_seen(sys.argv[1], i)
-            write_pickled_file(f"data/ccs_data_{sys.argv[1]}", warzone_data)
-            time_str = "[" + datetime.now().isoformat() + "]".format(type)
-            log_message(
-                f'Finished parsing {i=} and found {len(warzone_data)} matches. Progress: {(i-cl_info["start"])/(cl_info["end"]-cl_info["start"])*100}',
-                "CCS",
-            )
-
-        game_data = api.query_game_full(i)
-        if game_data and cl_info["matcher"](game_data):
-            groups = cl_info["parser"](game_data)
-            warzone_data.append(
-                ClotGame(
-                    *groups,
-                    game_data.link,
-                    game_data.players,
-                    game_data.winner,
-                    game_data.start_time,
-                    game_data.round,
+if sys.argv[2] == "scrape":
+    try:
+        for i in range(last_seen_index, cl_info["end"]):
+            if i % 1000 == 0:
+                # Save entries every 1000 lines
+                overwrite_index_seen(sys.argv[1], i)
+                write_pickled_file(f"data/ccs_data_{sys.argv[1]}", warzone_data)
+                time_str = "[" + datetime.now().isoformat() + "]".format(type)
+                log_message(
+                    f'Finished parsing {i=} and found {len(warzone_data)} matches. Progress: {(i-cl_info["start"])/(cl_info["end"]-cl_info["start"])*100}',
+                    f"CCS_{sys.argv[1]}",
                 )
-            )
-except Exception as e:
-    overwrite_index_seen(sys.argv[1], i)
-    write_pickled_file(f"data/ccs_data_{sys.argv[1]}", warzone_data)
-    log_message(
-        f'Finished parsing {i=}. Progress: {(i-cl_info["start"])/(cl_info["end"]-cl_info["start"])*100}',
-        "CCS",
+
+            game_data = api.query_game_full(i)
+            if game_data and cl_info["matcher"](game_data):
+                groups = cl_info["parser"](game_data)
+                warzone_data.append(
+                    ClotGame(
+                        *groups,
+                        game_data.link,
+                        game_data.players,
+                        game_data.winner,
+                        game_data.start_time,
+                        game_data.round,
+                    )
+                )
+    except Exception as e:
+        overwrite_index_seen(sys.argv[1], i)
+        write_pickled_file(f"data/ccs_data_{sys.argv[1]}", warzone_data)
+        log_message(
+            f'Finished parsing {i=}. Progress: {(i-cl_info["start"])/(cl_info["end"]-cl_info["start"])*100}',
+            f"CCS_{sys.argv[1]}",
+        )
+        log_exception(e)
+elif sys.argv[2] == "parse":
+    lines_to_output = []
+    for game in warzone_data:
+        lines_to_output.append(
+            [
+                game.cl,
+                game.division,
+                game.template,
+                game.link,
+                game.start_time,
+                *format_players_to_array(game.players),
+            ]
+        )
+
+    lines_to_output.sort(
+        key=lambda x: (x[0], x[1], x[2], x[6], x[8], x[10], x[12], x[14], x[16])
     )
-    log_exception(e)
+    with open(f"data/ccs_tabbed_output_{sys.argv[1]}.csv", "w") as f:
+        f.write(
+            "\n".join(["\t".join([str(x) for x in line]) for line in lines_to_output])
+        )
